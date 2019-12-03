8ad2e919f4a9ce1cec3a0c93df198408ce538033
Shalom91 <56162445+Shalom91@users.noreply.github.com>
2019-12-03 15:45:55 +0200

    Added Second Exercise

---
+++ odd_even.py
@@ -0,0 +1,11 @@
+# This program asks the user for a number and determines if the 
+# number is an odd or even number
+
+# ask the user to input a number
+num = int(input('Please enter a any number: '))
+
+# use conditional statements and the modular operator
+if num % 2 == 0:
+    print('This is an even number')
+else:
+    print('This is an odd number')
